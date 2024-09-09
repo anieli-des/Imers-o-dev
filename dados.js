7c2182275c25c53d4fb17331bb7c1afa8182a647
anieli-des <anielilondero1@gmail.com>
2024-09-08 22:22:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo : "Gabriela Guimarães",
+        descricao : "Gabriela Guimarães, é uma das maiores jogadoras de vôlei do Brasil e do mundo. Com sua potência nos ataques e habilidade técnica, ela se tornou peça fundamental da seleção brasileira.",
+        link : "https://pt.wikipedia.org/wiki/Gabi_Guimar%C3%A3es",
+        tags:"gabi filha do deus do vôlei gabizinha medalista"
+    },
+    {
+        titulo : "Paola Egonu",
+        descricao : "Paola Egonu é uma das maiores revelações do vôlei mundial. A atacante italiana, com sua força física e habilidade nos ataques, se tornou um dos nomes mais importantes do esporte. Sua versatilidade e carisma a tornaram um ícone do vôlei feminino.",
+        link : "https://pt.wikipedia.org/wiki/Paola_Egonu",
+        tags:"da silva egonu italiana medalista"
+    },
+    {
+        titulo : "Fernanda Garay",
+        descricao : "Fernanda Garay é uma das maiores referências do vôlei brasileiro. A ponteira, conhecida por sua força e experiência, conquistou diversos títulos ao longo de sua carreira. Sua liderança dentro de quadra e sua história de superação inspiram muitos fãs.",
+        link : "https://pt.wikipedia.org/wiki/Fernanda_Garay",
+        tags:"fe joga pra garay brasileira medalista"
+    },
+    {
+        titulo : "Carol Gattaz",
+        descricao : "Carol Gattaz é uma central experiente e carismática. Com seu bloqueio sólido e seus ataques eficientes, ela se tornou uma referência na posição. Sua liderança e sua alegria contagiante dentro de quadra inspiram seus companheiros de equipe.",
+        link : "https://pt.wikipedia.org/wiki/Carol_Gattaz",
+        tags:"gattaz china minas rosattaz"
+    },
+    {
+        titulo : "Ana Carolina da Silva",
+        descricao : "Carol se destaca por sua versatilidade e habilidade em diversas posições na quadra. Sua trajetória no vôlei demonstra sua paixão pelo esporte e sua dedicação aos treinos. Com sua técnica apurada e espírito de equipe, Carolana se tornou uma peça fundamental em diversas equipes.",
+        link : "https://pt.wikipedia.org/wiki/Ana_Carolina_(voleibolista)",
+        tags:"carolana 15 praia brasileira"
+    },
+    {
+        titulo : "Tijana Bošković",
+        descricao : "Tijana Bošković é reconhecida mundialmente por sua força e precisão nos ataques. Considerada uma das melhores opostas do mundo, ela se destaca por sua capacidade de definir os pontos e por sua liderança em quadra. Sua presença imponente e seu talento inegável a tornaram uma das atletas mais admiradas do vôlei feminino.",
+        link : "https://pt.wikipedia.org/wiki/Tijana_Bo%C5%A1kovi%C4%87",
+        tags:"servia"
+    },
+];
